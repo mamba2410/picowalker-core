90ebeb9dd54f8d4b7b608f6b4e021877c311762f
Mamba2410 <callummcgregor54@gmail.com>
2023-08-02 14:20:18 +0100

    buffers: shuffle buffers around to avoid overflow

--- src/globals.c
+++ src/globals.c
@@ -4,10 +4,11 @@
 #include "types.h"
 
 
-uint8_t eeprom_buf[EEPROM_BUF_SIZE];
-uint8_t decompression_buf[DECOMPRESSION_BUF_SIZE];
-pw_packet_t packet_buf;
-
 health_data_t health_data_cache;
 walker_info_t walker_info_cache;
 walker_info_t peer_info_cache;
+pw_packet_t packet_buf;
+
+uint8_t eeprom_buf[EEPROM_BUF_SIZE];
+uint8_t decompression_buf[DECOMPRESSION_BUF_SIZE];
+
--- src/globals.h
+++ src/globals.h
@@ -7,17 +7,21 @@
 
 /// @file globals.h
 
-extern uint8_t eeprom_buf[];
-extern uint8_t decompression_buf[];
-extern pw_packet_t packet_buf;
+#define EEPROM_BUF_SIZE         0x300   // largest image to read is 96x32=0x300 bytes
+#define DECOMPRESSION_BUF_SIZE  (2*EEPROM_BUF_SIZE)
+//#define DECOMPRESSION_BUF_SIZE  0x100
+#define PACKET_BUF_SIZE         0x88
+
 
 extern health_data_t health_data_cache;
 extern walker_info_t walker_info_cache;
 extern walker_info_t peer_info_cache;
+extern pw_packet_t packet_buf;
 
-#define EEPROM_BUF_SIZE         0x100
-#define DECOMPRESSION_BUF_SIZE  0x100
-#define PACKET_BUF_SIZE         0x88
+extern uint8_t eeprom_buf[];
+extern uint8_t decompression_buf[];
+
+
 
 
 #endif /* PW_BUFFERS_H */
