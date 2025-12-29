2e4e8522c25ace6c2943651a3ee82266d89c55cf
mamba2410 <callummcgregor54@gmail.com>
2025-12-29 22:55:11 +0000

    picowalker: add more picowalker sprites

--- src/pico_roms.h
+++ src/pico_roms.h
@@ -2,6 +2,9 @@
 #define PICO_ROMS_H
 
 extern uint8_t sad_pokewalker[];
-extern uint8_t picowalker_text[];
+extern uint8_t picowalker_fancy_text[];
+extern uint8_t picowalker_border_text[];
+extern uint8_t battery_fancy_text[];
+extern uint8_t percent_char[];
 
 #endif /* PICO_ROMS_H */
